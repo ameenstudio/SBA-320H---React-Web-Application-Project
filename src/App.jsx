9224c7a5fa19d127607718107fd38750c29cb09f
ameenstudio <200854608+ameenstudio@users.noreply.github.com>
2025-05-19 15:52:28 -0600

    routes are built paths are woking!!

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import './App.css'
+import { Routes, Route } from 'react-router-dom'
+import Homepage from './pages/Homepage'
+import Characters from './pages/Characteres'
+import Episodes from './pages/Episodes';
+import Locations from './pages/Locations';
+import NotFound from './pages/NotFound';
 function App() {
   
 
   return (
     <>
-      
+      <Routes>
+       <Route path="/" element={<Homepage />} />
+        <Route path="/characters" element={<Characters />} />
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/locations" element={<Locations />} />
+        <Route path="*" element={<NotFound/>}/>
+      </Routes>
     </>
   )
 }
